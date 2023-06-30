e2b772ef39b155e2210738c05257f3c020c62048
itsluismario <itsluismario@gmail.com>
2023-06-30 11:10:44 -0600

    :sparkles: Routing

--- index.js
+++ index.js
@@ -7,6 +7,23 @@ app.get('/', (req, res) => {
   res.send('Hi, my server in express');
 });
 
+app.get('/products', (req, res) => {
+  res.json(
+    {
+      name: 'P1',
+      price: 23238
+    },
+    )
+});
+
+app.get('/categories', (req, res) => {
+  res.json(
+    {
+      category: 'C1',
+    },
+    )
+});
+
 // You must neve user console.log in production, only for dev
 app.listen(port, () => {
   console.log('My port' + port);
